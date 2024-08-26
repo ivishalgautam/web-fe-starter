512725b7dede34b5bac0c979ea11d78877eaf41a
Vishal Gautam <ivishalgautamm@gmail.com>
2024-08-26 11:35:14 +0530

    init

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,64 +4,15 @@ import QueryProvider from "@/components/QueryClientProvider";
 import { Toaster } from "sonner";
 import Context from "@/store/context";
 import Layout from "@/components/layout";
-import { GoogleTagManager } from "@next/third-parties/google";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css/autoplay";
 
 export const metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL),
-  title: {
-    default:
-      "InfraKeys: Stainless Steel l Wire Mesh & Scaffolding Manufacturer in India",
-    // template: "%s | Infrakeys",
-  },
-  description:
-    "Infrakeys, the best steel manufacturers in India, offered a wide range of steel, wire mesh & binding wire, scaffolding, doors & windows, nails & industrial products at reasonable prices.",
-  keywords:
-    "Steel Manufacturers in india, steel fabricators near me,industrial steel in faridabad, Steel supplier in India, scaffolding manufacturer in faridabad, TMT suppliers in faridabad, PEB manufacturers in India, PEB manufacturers, steel supplier in faridabad, steel manufacturers,india, industrial steel in india, scaffolding manufacturer in India, TMT suppliers in India, steel manufacturers india",
-  alternates: {
-    canonical: `/`,
-  },
-  openGraph: {
-    title:
-      "InfraKeys: Stainless Steel l Wire Mesh & Scaffolding Manufacturer in India",
-    description:
-      "Infrakeys, the best steel manufacturers in India, offered a wide range of steel, wire mesh & binding wire, scaffolding, doors & windows, nails & industrial products at reasonable prices.",
-    images: [
-      {
-        url: "https://www.infrakeys.com/_next/static/media/banner-1.df4039c7.jpg",
-        width: 800,
-        height: 600,
-        alt: "Infrakeys",
-      },
-      {
-        url: "https://www.infrakeys.com/_next/static/media/banner-2.e2d2ad9e.jpg",
-        width: 1800,
-        height: 1600,
-        alt: "Infrakeys",
-      },
-    ],
-  },
-  verification: {
-    google: "kWcwy0Kag9MmpnCSMcrOL7VuQT5ZKjuBbZ6218QCpZw",
-  },
-  icons: {
-    icon: "/favicon.ico",
-  },
+  // metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL),
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <GoogleTagManager gtmId="GTM-WN9SB6DD" />
-        <GoogleTagManager gtmId="AW-11427244694" />
-      </head>
+      <head></head>
       <body
         className={`${GeistSans.className} overflow-x-hidden`}
         suppressHydrationWarning={true}
